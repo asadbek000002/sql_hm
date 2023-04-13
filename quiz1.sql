f24081a9dbf468f718db582657846c4b29e18750
asadbek000002 <asadbektuygunov9@gmail.com>
2023-04-13 05:19:49 +0500

    quiz1 javoblari

--- quiz1.sql
+++ quiz1.sql
@@ -8,3 +8,14 @@
 
 --select  count(recipe_classes.recipeclassdescription) from recipes inner join recipe_classes
 --using (recipeclassid)
+
+--3
+
+--select bowlers.bowlerlastname, teams.teamname
+--from bowlers
+--inner join teams on bowlers.teamid = teams.teamid
+--where bowlerlastname like 'H%' and teams.teamid in (3,4,5)
+
+--4
+
+--select recipetitle, notes from recipes where notes is null
